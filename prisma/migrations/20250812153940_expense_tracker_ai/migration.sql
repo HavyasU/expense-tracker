0da8d424d03bc853d688e2bc6ef6e66e0d3c1347
HavyasU <havyasu2005@gmail.com>
2025-08-12 21:17:39 +0530

    install neon and prisma

---
+++ prisma/migrations/20250812153940_expense_tracker_ai/migration.sql
@@ -0,0 +1,37 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" TEXT NOT NULL,
+    "clerkUserId" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "imageUrl" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Record" (
+    "id" TEXT NOT NULL,
+    "text" TEXT,
+    "amount" DOUBLE PRECISION,
+    "category" TEXT NOT NULL DEFAULT 'Other',
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Record_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_clerkUserId_key" ON "public"."User"("clerkUserId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
+
+-- CreateIndex
+CREATE INDEX "Record_id_idx" ON "public"."Record"("id");
+
+-- AddForeignKey
+ALTER TABLE "public"."Record" ADD CONSTRAINT "Record_id_fkey" FOREIGN KEY ("id") REFERENCES "public"."User"("clerkUserId") ON DELETE CASCADE ON UPDATE CASCADE;
